e2006f2cc1673370ce6ed361e563053df967d0a5
Víctor Delgado Batista <63307570+Vic-28@users.noreply.github.com>
2024-06-21 20:19:13 +0100

    Agregado de footer y header

---
+++ fincaTamaide/src/components/shared/footer/footer.tsx
@@ -0,0 +1,13 @@
+import './footer.css';
+
+
+function Footer() {
+
+    return (
+     <>
+     <h1>Apartado del footer</h1>
+  
+     </>
+    )
+  }
+export default Footer
---
+++ fincaTamaide/src/components/shared/header/header.tsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+
+function Header() {
+
+    return (
+     <>
+     <h1>Apartado del header</h1>
+  
+     </>
+    )
+  }
+export default Header
