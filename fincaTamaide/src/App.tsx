cffe94ff6741396fa21509e285ea1e4214ee850c
Víctor Delgado Batista <63307570+Vic-28@users.noreply.github.com>
2024-06-21 20:23:32 +0100

    Agregado del sashboard e imagenes

--- fincaTamaide/src/App.tsx
+++ fincaTamaide/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './components/shared/header/header'
 import Footer from './components/shared/footer/footer'
+import Dashboard from './components/dashboard/dashboard'
 
 function App() {
 
   return (
    <>
     <Header />
-   <h1>Apartado vacio</h1>
+    <Dashboard />
     <Footer />
    </>
   )
---
+++ fincaTamaide/src/components/dashboard/dashboard.tsx
@@ -0,0 +1,14 @@
+import './dashboard.css'
+
+
+function Dashboard() {
+
+    return (
+     <>
+     <h1>Apartado del dashboard</h1>
+  
+     </>
+    )
+  }
+
+  export default Dashboard
