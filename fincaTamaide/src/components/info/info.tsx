e7ee455c16d9706f87163f94abb582bdcdb5aa39
Víctor Delgado Batista <63307570+Vic-28@users.noreply.github.com>
2024-06-22 01:23:04 +0100

    Cambio de diseño info derecha

--- fincaTamaide/src/components/info/info.tsx
+++ fincaTamaide/src/components/info/info.tsx
@@ -9,24 +9,24 @@ function Info()
                     <img className='info-card-image left' src='https://i.postimg.cc/J4WyDJF9/IMG-8109.avif' alt='info-card-left-image'/>
                     <div className='info-card-left-content'>
                         <h5 className='info-card-left-content-title'>Experiencias únicas</h5>
-                        <div className='info-card-left-content-separator' />
-                        <p>Con las mejores comodidades</p>
+                        <div className='info-card-content-separator' />
+                        <p>Entorno perfecto para disfrutar del clima de Tenerife</p>
                     </div>
                 </section>
                 <section className='info-card-right'>
                     <div className='info-card-right-content'>
-                        <h5 className='info-card-right-content-title'>Experiencias únicas</h5>
-                        <div className='info-card-right-content-separator' />
-                        <p>Con las mejores comodidades</p>
+                        <h5 className='info-card-right-content-title'>Vistas perfectas</h5>
+                        <div className='info-card-content-separator' />
+                        <p>Todo lo necesario para un descanso sin molestias</p>
                     </div>
                     <img className='info-card-image right' src='https://i.postimg.cc/rF8V293F/IMG-8138.avif' alt='info-card-right-image'/>
                 </section>
                 <section className='info-card-left'>
                     <img className='info-card-image left' src='https://i.postimg.cc/rsVv3fmB/IMG-8174.avif' alt='info-card-left-image'/>
                     <div className='info-card-left-content'>
-                        <h5 className='info-card-left-content-title'>Experiencias únicas</h5>
-                        <div className='info-card-left-content-separator' />
-                        <p>Con las mejores comodidades</p>
+                        <h5 className='info-card-left-content-title'>Parte exterior ideal</h5>
+                        <div className='info-card-content-separator' />
+                        <p>Con todo lo necesario para un buen día de piscina</p>
                     </div>
                 </section>
             </aside>
