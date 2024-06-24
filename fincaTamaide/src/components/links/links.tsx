7f6aa749450691d6597a4b48c090b5966672f938
Víctor Delgado Batista <63307570+Vic-28@users.noreply.github.com>
2024-06-24 21:24:38 +0100

    cambios en el footer

--- fincaTamaide/src/components/links/links.tsx
+++ fincaTamaide/src/components/links/links.tsx
@@ -3,7 +3,7 @@ import './links.css';
 import airbnbLogo from '/src/assets/img/airbnb.png';
 import bookingLogo from '/src/assets/img/brand-booking.svg';
 
-function Links() {
+const Links: React.FC = () => {
     useEffect(() => {
         const navElement = document.querySelector('.links-nav.link-non-active');
         if (!navElement) return;
@@ -12,13 +12,32 @@ function Links() {
             navElement.classList.toggle('link-active');
             navElement.classList.toggle('link-non-active');
         };
-        
+
+        const handleCloseClick: EventListener = (event) => {
+            event.stopPropagation(); // Prevent triggering the parent click event
+            navElement.classList.add('link-non-active');
+            navElement.classList.remove('link-active');
+        };
+
         navElement.addEventListener('click', handleClick);
+
+        const closeIcon = navElement.querySelector('.links-nav-icono-salida');
+        if (closeIcon) {
+            closeIcon.addEventListener('click', handleCloseClick);
+        }
 
         return () => {
             navElement.removeEventListener('click', handleClick);
+            if (closeIcon) {
+                closeIcon.removeEventListener('click', handleCloseClick);
+            }
         };
     }, []);
+
+    const goToAirbnb = (event: React.MouseEvent<HTMLImageElement>) => {
+        event.stopPropagation();
+        window.location.href = 'https://www.airbnb.es/rooms/49413191?adults=1&children=0&enable_m3_private_room=true&infants=0&location=San%20Miguel%20de%20Abona%2C%20Espa%C3%B1a&pets=0&search_mode=regular_search&check_in=2024-07-01&check_out=2024-07-06&source_impression_id=p3_1719072932_P3UItdNMq6t-kCJr&previous_page_section_name=1001&federated_search_id=97963ca0-f4af-40b7-87a7-e00d46297100&translate_ugc=false';
+    };
 
     return (
         <aside className='links'>
@@ -29,8 +48,18 @@ function Links() {
                 <span className="material-symbols-outlined links-nav-icono-salida">
                     close
                 </span>
-                <img className='links-nav-logo' src={airbnbLogo} alt='Airbnb logo' />
-                <img className='links-nav-logo' src={bookingLogo} alt='Booking logo' />
+                <img
+                    className='links-nav-logo'
+                    src={airbnbLogo}
+                    alt='Airbnb logo'
+                    onClick={goToAirbnb}
+                />
+                <img
+                    className='links-nav-logo'
+                    src={bookingLogo}
+                    alt='Booking logo'
+                    style={{ display: 'none'}}
+                />
             </nav>
         </aside>
     );
--- fincaTamaide/src/components/shared/footer/footer.tsx
+++ fincaTamaide/src/components/shared/footer/footer.tsx
@@ -1,14 +1,14 @@
 import './footer.css';
 
-
 function Footer() {
-
     return (
-     <footer>
-
-      <h1>Prueba de texto del footer</h1>
-  
-     </footer>
+        <footer>
+            <h6><a href='https://www.fincatamaide.es'>fincatamaide.es</a></h6>
+            <p> &copy; 2024</p>
+            <p> Página Hecha por Víctor Delgado Batista</p>
+            <p>Icons from <a href='https://www.flaticon.es/'>https://www.flaticon.es/</a> & <a href='https://fonts.google.com/icons'>https://fonts.google.com/icons</a> </p>
+        </footer>
     )
-  }
-export default Footer
+}
+
+export default Footer;
