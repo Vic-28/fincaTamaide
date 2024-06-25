d634c39ae8a692a670e4116eef44422d41d8ac8a
Víctor Delgado Batista <63307570+Vic-28@users.noreply.github.com>
2024-06-25 18:16:35 +0100

    primera version movil final

--- fincaTamaide/src/App.tsx
+++ fincaTamaide/src/App.tsx
@@ -6,11 +6,11 @@ import Dashboard from './components/dashboard/dashboard'
 function App() {
 
   return (
-   <>
+   <section className='app'>
     <Header />
     <Dashboard />
     <Footer />
-   </>
+   </section>
   )
 }
 
